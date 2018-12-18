0590593ce210123fab6b2414c454a7a83a1c801e
Oscar Pachano <opachano@gmail.com>
2018-12-18 18:55:36 -0500

    Adds big object with all the location details

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser  = require("cookie-parser");
 const express       = require("express");
 const mongoose      = require("mongoose");
 const logger        = require("morgan");
+const hbs           = require("hbs");
 const path          = require("path");
 const session       = require("express-session");
 const passport      = require("passport");
@@ -29,6 +30,16 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
+      
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: "our-passport-local-strategy-app",
@@ -55,6 +66,6 @@ app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/trip", require("./routes/tripRoutes"));
 app.use("/api/review", require("./routes/reviewRoutes"));
 app.use("/api/detail", require("./routes/detailRoutes"));
-app.use("api/google", require("./routes/googleRoutes"));
+app.use("/api/google", require("./routes/googleRoutes"));
 
 module.exports = app;
--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const router  = express.Router();
-const Axios   = require("axios")
+const axios   = require("axios")
 
-router.get("/:city", (req, res, next)=> {
-  Axios.post(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=hotels+${req.params.city}&key=${process.env.GOOGLE_KEY}`)
+router.post("/locationInfo", (req, res, next)=> {
+  axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=hotels+${req.body.city}&key=${process.env.GOOGLE_KEY}`)
   .then((hotels)=>{
-    res.json(hotels)
+    axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+${req.body.city}&key=${process.env.GOOGLE_KEY}`)
+    .then((restaurants)=>{
+      axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=activities+${req.body.city}&key=${process.env.GOOGLE_KEY}`)
+      .then((activities)=>{
+        hotels2 = hotels.data.results.map((result)=>{
+          if(result.photos){
+            result.photos = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos["0"].photo_reference}&key=${process.env.GOOGLE_KEY}`
+            return result
+          }
+        })
+        restaurants2 = restaurants.data.results.map((result)=>{
+          if(result.photos){
+            result.photos = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos["0"].photo_reference}&key=${process.env.GOOGLE_KEY}`
+            return result
+          }
+        })
+        activities2 = activities.data.results.map((result)=>{
+          if(result.photos){
+            result.photos = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos["0"].photo_reference}&key=${process.env.GOOGLE_KEY}`
+            return result
+          }
+        })
+        res.json({
+          hotels: hotels2,
+          restaurants: restaurants2,
+          activities: activities2
+        })
+      })
+      .catch((err)=>{
+        res.json(err)
+      })
+    })
+    .catch((err)=>{
+      res.json(err)
+    })
   })
   .catch((err)=>{
     res.json(err)
